ba24bcd258d7260809caa9131cbe9b0fa4697527
AdriVanHoudt <adri.van.houdt@gmail.com>
2013-06-24 12:10:50 +0200

    reformat file

--- Python/google-python-exercises/hello.py
+++ Python/google-python-exercises/hello.py
@@ -21,81 +21,81 @@ import sys
 
 # Define a main() function that prints a little greeting.
 def main():
-    ## Get the name from the command line, using 'World' as a fallback.
-    if len(sys.argv) >= 2:
-        name = sys.argv[1]
-    else:
-        name = 'World'
-    print 'Howdy', name
+  ## Get the name from the command line, using 'World' as a fallback.
+  if len(sys.argv) >= 2:
+    name = sys.argv[1]
+  else:
+    name = 'World'
+  print 'Howdy', name
 
-    print repeat('Adri', True, 3)
+  print repeat('Adri', True, 3)
 
-    ## string test
-    s = 'hi'
-    print s[1]
-    print len(s)
-    print s + ' there'
+  ## string test
+  s = 'hi'
+  print s[1]
+  print len(s)
+  print s + ' there'
 
-    pi = 3.14
-    ## print 'the value of pi is ' + pi ## doesn't work
-    print 'the value of pi is ' + str(pi)
+  pi = 3.14
+  ## print 'the value of pi is ' + pi ## doesn't work
+  print 'the value of pi is ' + str(pi)
 
-    print 6 // 5  # double slashes because logic
+  print 6 // 5  # double slashes because logic
 
-    ## test for raw and multi strings
-    raw = r'this\t\n and that'
-    ## tab before second line is also added!
-    multi = """It was the best of times.
+  ## test for raw and multi strings
+  raw = r'this\t\n and that'
+  ## tab before second line is also added!
+  multi = """It was the best of times.
     It was the worst of times."""
 
-    print raw
-    print multi
+  print raw
+  print multi
 
-    print raw  # this\t\n and that
+  print raw  # this\t\n and that
 
-    ## Methods
-    s = '555'
-    if s.isdigit():
-        print s
-    s = 'some text just for test'
-    print s.find('text')
+  ## Methods
+  s = '555'
+  if s.isdigit():
+    print s
+  s = 'some text just for test'
+  print s.find('text')
 
-    s = 'Hello'
-    print s[:len(s)] + s[len(s):]
+  s = 'Hello'
+  print s[:len(s)] + s[len(s):]
 
-    ## %
-    print "%d little pigs come out or I'll %s and %s and %s" % (3, 'huff', 'puff', 'blow down')
+  ## %
+  print "%d little pigs come out or I'll %s and %s and %s" % (3, 'huff', 'puff', 'blow down')
 
-    ## if then else elif
-    speed = 90
-    mood = 'bad'
+  ## if then else elif
+  speed = 90
+  mood = 'bad'
 
-    if speed >= 80:
-        print 'License and registration please.'
-        if mood == 'terrible' or speed >= 100:
-            print 'You have the right to remain silent.'
-        elif mood == 'bad' or speed >= 90:
-            print "I'm going to have to write a ticket."
-            write_ticket()
-        else:
-            print "Let's try to keep it under 80 ok?"
+  if speed >= 80:
+    print 'License and registration please.'
+    if mood == 'terrible' or speed >= 100:
+      print 'You have the right to remain silent.'
+    elif mood == 'bad' or speed >= 90:
+      print "I'm going to have to write a ticket."
+      write_ticket()
+    else:
+      print "Let's try to keep it under 80 ok?"
 
 
-    ## sys import test
-    sys.exit(0)
+  ## sys import test
+  sys.exit(0)
 
 # write_ticket for if then else statement
 def write_ticket():
-    print 'Ticket :O'
+  print 'Ticket :O'
 
 # defining function test
 def repeat(s, exclaim, times):
-    result = s * times
+  result = s * times
 
-    if exclaim:
-        result += '!!!'
-    return result
+  if exclaim:
+    result += '!!!'
+  return result
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
-    main()
+  main()
