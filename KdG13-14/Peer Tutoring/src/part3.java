7916dfc49c4cc6ce1585ce7a4a84abd355c6f2e6
AdriVanHoudt <adri.van.houdt@gmail.com>
2013-09-27 16:50:39 +0200

    added part 3

---
+++ KdG13-14/Peer Tutoring/src/part3.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * User: Adri
+ * Date: 27/09/13
+ * Time: 16:10
+ */
+public class part3 {
+    public static void main(String[] args) {
+        int start, end, step;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Geef de begintemperatuur in °C: ");
+        start =  scanner.nextInt();
+        System.out.print("Geef de eindtemperatuur in °C: ");
+        end = scanner.nextInt();
+
+        if (end < start){
+            System.out.println("De eindwaarde moet groter zijn dan de beginwaarde.");
+        }
+
+        if (end > start){
+
+            System.out.print("Geef de stopwaarde: ");
+            step = scanner.nextInt();
+
+            while (start <= end){
+                double result = start * 1.8 + 32 ;
+                System.out.printf("10%s°C = " + result + "°F", "" + start);
+                start += step;
+            }
+        }
+    }
+}
