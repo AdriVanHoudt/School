68d67933ca6b7da3dad092f8e1d90aa061f00f3c
AdriVanHoudt <adri.van.houdt@gmail.com>
2013-10-14 18:40:40 +0200

    added api docs, just because; some work towards jsp pages

--- KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/BlogListServlet.java
+++ KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/BlogListServlet.java
@@ -36,71 +36,74 @@ public class BlogListServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletContext cntxt = this.getServletContext();
-
-        Blog blog = (Blog) cntxt.getAttribute("blog");
-
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>" + ((User) request.getSession().getAttribute("user")).getUsername() + "</h1>");
-            out.println("<table border=1>");
-
-            Map posts = blog.getPosts();
-
-            Iterator it = posts.entrySet().iterator();
-
-            while (it.hasNext()) {
-                Map.Entry pairs = (Map.Entry) it.next();
-                Post post = (Post) pairs.getValue();
-
-                String colorcode = "#FFFFF";
-                if (!it.hasNext()) {
-                    colorcode = "#FFE473";
-                }
-
-                //posted by
-                out.println("<tr bgcolor=" + colorcode + ">");
-                out.println("<td>posted by</td>");
-                out.println("<td>" + post.getUser().getUsername() + "</td>");
-                out.println("</tr>");
-                //year
-                out.println("<tr bgcolor=" + colorcode + ">");
-                out.println("<td>jaar</td>");
-                out.println("<td>" + post.getYear() + "</td>");
-                out.println("</tr>");
-                //Specialization
-                out.println("<tr bgcolor=" + colorcode + ">");
-                out.println("<td>specializatie</td>");
-                out.println("<td>" + post.getSpecialization() + "</td>");
-                out.println("</tr>");
-                //description
-                out.println("<tr bgcolor=" + colorcode + ">");
-                out.println("<td>omschrijving</td>");
-                out.println("<td>" + post.getDescription() + "</td>");
-                out.println("</tr>");
-                //url
-                out.println("<tr bgcolor=" + colorcode + ">");
-                out.println("<td> url</td>");
-                out.println("<td><a href='http://" + post.getUrl() + "'>" + post.getUrl() + "</a></td>");
-                out.println("</tr>");
-
-            }
-
-            out.println("</table>");
-            //link to blogpost
-            out.println("<a href='blogpost.html'>Add a new post</a>");
-            out.println("<a href='LogOutServlet'>Log Out</a>");
-
-            out.println("</body>");
-            out.println("</html>");
-        }
+        Blog blog = (Blog) this.getServletContext().getAttribute("blog");
+        request.setAttribute("posts", blog.getPosts());
+        
+//        ServletContext cntxt = this.getServletContext();
+//
+//        Blog blog = (Blog) cntxt.getAttribute("blog");
+//
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet NewServlet</title>");
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>" + ((User) request.getSession().getAttribute("user")).getUsername() + "</h1>");
+//            out.println("<table border=1>");
+//
+//            Map posts = blog.getPosts();
+//
+//            Iterator it = posts.entrySet().iterator();
+//
+//            while (it.hasNext()) {
+//                Map.Entry pairs = (Map.Entry) it.next();
+//                Post post = (Post) pairs.getValue();
+//
+//                String colorcode = "#FFFFF";
+//                if (!it.hasNext()) {
+//                    colorcode = "#FFE473";
+//                }
+//
+//                //posted by
+//                out.println("<tr bgcolor=" + colorcode + ">");
+//                out.println("<td>posted by</td>");
+//                out.println("<td>" + post.getUser().getUsername() + "</td>");
+//                out.println("</tr>");
+//                //year
+//                out.println("<tr bgcolor=" + colorcode + ">");
+//                out.println("<td>jaar</td>");
+//                out.println("<td>" + post.getYear() + "</td>");
+//                out.println("</tr>");
+//                //Specialization
+//                out.println("<tr bgcolor=" + colorcode + ">");
+//                out.println("<td>specializatie</td>");
+//                out.println("<td>" + post.getSpecialization() + "</td>");
+//                out.println("</tr>");
+//                //description
+//                out.println("<tr bgcolor=" + colorcode + ">");
+//                out.println("<td>omschrijving</td>");
+//                out.println("<td>" + post.getDescription() + "</td>");
+//                out.println("</tr>");
+//                //url
+//                out.println("<tr bgcolor=" + colorcode + ">");
+//                out.println("<td> url</td>");
+//                out.println("<td><a href='http://" + post.getUrl() + "'>" + post.getUrl() + "</a></td>");
+//                out.println("</tr>");
+//
+//            }
+//
+//            out.println("</table>");
+//            //link to blogpost
+//            out.println("<a href='blogpost.html'>Add a new post</a>");
+//            out.println("<a href='LogOutServlet'>Log Out</a>");
+//
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
 
     }
 }
--- KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/LogOutServlet.java
+++ KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/LogOutServlet.java
@@ -25,10 +25,8 @@ public class LogOutServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       request.getSession().removeAttribute("user");
-       request.getSession().invalidate();
-       response.sendRedirect("logoff.html");
+        request.getSession().removeAttribute("user");
+        request.getSession().invalidate();
+        response.sendRedirect("logoff.html");
     }
-
-
 }
