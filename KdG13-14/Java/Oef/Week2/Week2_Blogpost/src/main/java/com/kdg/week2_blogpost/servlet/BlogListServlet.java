38231bb96c3d5d7d4bb7e9c29dca6eaeec02ca44
AdriVanHoudt <adri.van.houdt@gmail.com>
2013-10-25 15:46:02 +0200

    start work on filter

--- KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/BlogListServlet.java
+++ KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogpost/servlet/BlogListServlet.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kdg.week2_blogpost.servlet;
 
 import com.kdg.week2_blogpost.model.Blog;
@@ -34,71 +30,6 @@ public class BlogListServlet extends HttpServlet {
         request.setAttribute("posts", blog.getPosts().values());
         
         request.getRequestDispatcher("bloglist.jsp").forward(request, response);
-//        ServletContext cntxt = this.getServletContext();
-//
-//        Blog blog = (Blog) cntxt.getAttribute("blog");
-//
-//        response.setContentType("text/html;charset=UTF-8");
-//        try (PrintWriter out = response.getWriter()) {
-//
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet NewServlet</title>");
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>" + ((User) request.getSession().getAttribute("user")).getUsername() + "</h1>");
-//            out.println("<table border=1>");
-//
-//            Map posts = blog.getPosts();
-//
-//            Iterator it = posts.entrySet().iterator();
-//
-//            while (it.hasNext()) {
-//                Map.Entry pairs = (Map.Entry) it.next();
-//                Post post = (Post) pairs.getValue();
-//
-//                String colorcode = "#FFFFF";
-//                if (!it.hasNext()) {
-//                    colorcode = "#FFE473";
-//                }
-//
-//                //posted by
-//                out.println("<tr bgcolor=" + colorcode + ">");
-//                out.println("<td>posted by</td>");
-//                out.println("<td>" + post.getUser().getUsername() + "</td>");
-//                out.println("</tr>");
-//                //year
-//                out.println("<tr bgcolor=" + colorcode + ">");
-//                out.println("<td>jaar</td>");
-//                out.println("<td>" + post.getYear() + "</td>");
-//                out.println("</tr>");
-//                //Specialization
-//                out.println("<tr bgcolor=" + colorcode + ">");
-//                out.println("<td>specializatie</td>");
-//                out.println("<td>" + post.getSpecialization() + "</td>");
-//                out.println("</tr>");
-//                //description
-//                out.println("<tr bgcolor=" + colorcode + ">");
-//                out.println("<td>omschrijving</td>");
-//                out.println("<td>" + post.getDescription() + "</td>");
-//                out.println("</tr>");
-//                //url
-//                out.println("<tr bgcolor=" + colorcode + ">");
-//                out.println("<td> url</td>");
-//                out.println("<td><a href='http://" + post.getUrl() + "'>" + post.getUrl() + "</a></td>");
-//                out.println("</tr>");
-//
-//            }
-//
-//            out.println("</table>");
-//            //link to blogpost
-//            out.println("<a href='blogpost.html'>Add a new post</a>");
-//            out.println("<a href='LogOutServlet'>Log Out</a>");
-//
-//            out.println("</body>");
-//            out.println("</html>");
-//        }
 
     }
 }
---
+++ KdG13-14/Java/Oef/Week2/Week2_Blogpost/src/main/java/com/kdg/week2_blogspost/filter/LoggingFilter.java
@@ -0,0 +1,37 @@
+package com.kdg.week2_blogspost.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author Adri
+ */
+public class LoggingFilter implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+         HttpServletRequest httpRequest = (HttpServletRequest) request;        
+       if(httpRequest.getMethod().equalsIgnoreCase("POST")){
+
+       }
+       chain.doFilter(request, response); 
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
